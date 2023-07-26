ccaa0974f560ef4fa2402832fd08a87130b2237e
Margarita Shumilina <margar.maksim@ya.ru>
2023-07-26 19:24:46 +0300

    третья итерация

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,13 +1,17 @@
 export const PAGINATION = {
-  DEFAULT_PAGE_ITEMS: 12,
-  DEFAULT_LOAD_ITEMS: 3,
+  DEFAULT_PAGE_ITEMS: 16,
+  DEFAULT_LOAD_ITEMS: 4,
   PAGE_ITEMS: {
     SCREEN: {
-      WIDTH_1280: {
+      WIDTH_1231: {
+        PAGE_ITEMS: 16,
+        LOAD_ITEMS: 4,
+      },
+      WIDTH_921: {
         PAGE_ITEMS: 12,
         LOAD_ITEMS: 3,
       },
-      WIDTH_768: {
+      WIDTH_731: {
         PAGE_ITEMS: 8,
         LOAD_ITEMS: 2,
       },
@@ -20,8 +24,9 @@ export const PAGINATION = {
 };
 
 export const SCREEN_RESOLUTION = {
-  WIDTH_1280: 1280,
-  WIDTH_768: 768,
+  WIDTH_1231: 1231,
+  WIDTH_921: 921,
+  WIDTH_731: 731,
   WIDTH_320: 320,
 };
 
